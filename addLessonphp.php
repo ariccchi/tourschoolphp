ad25389fdaf3dfb2ad7f75e88661e4da67125b73
Admin <admin@Admins-MacBook-Air.local>
2024-02-27 19:18:54 +0600

    addlessphp

--- addLessonphp.php
+++ addLessonphp.php
@@ -71,7 +71,47 @@ $stmtLessonId->fetch();
 $stmtLessonId->close();
 
 
-// Вставляем данные из infoarray в таблицу containers
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Обрабатываем каждый загруженный файл
+    foreach ($_FILES as $key => $file) {
+        // Извлекаем индекс из ключа
+        $index = str_replace('image_', '', $key);
+
+        // Проверяем, были ли ошибки при загрузке файла
+        if ($file['error'] == 0) {
+            // Генерируем уникальное имя для файла
+            $fileName = $file['name'];
+            $newFileName = 'img/' . md5(time() . $fileName) . '.' . pathinfo($fileName, PATHINFO_EXTENSION);
+
+            // Перемещаем файл в папку 'img'
+            $uploadPath = __DIR__ . '/' . $newFileName;
+            if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
+                // Файл успешно перемещен
+                echo "Файл $fileName успешно загружен.\n";
+
+                // Получаем время из POST-запроса
+                $timeInSeconds = $_POST['time_' . $index];
+                $formattedTime = gmdate("H:i:s", $timeInSeconds);
+
+                // Подготавливаем запрос на вставку данных в БД
+                $insertContainerQuery = "INSERT INTO containers (lesson_id, text, time_in) VALUES (?, ?, ?)";
+                $insertContainerStmt = $db->prepare($insertContainerQuery);
+
+                // Привязываем параметры и выполняем запрос
+                $insertContainerStmt->bind_param("iss", $lessonId, $newFileName, $formattedTime);
+                $insertContainerStmt->execute();
+
+                $insertContainerStmt->close();
+            } else {
+                // Во время перемещения файла произошла ошибка
+                echo "Не удалось переместить файл $fileName.\n";
+            }
+        } else {
+            // Во время загрузки файла произошла ошибка
+            echo "Ошибка при загрузке файла $fileName.\n";
+        }
+    }
+}
 if (!empty($infoarray)) {
     $insertContainerQuery = "INSERT INTO containers (lesson_id, text, time_in) VALUES (?, ?, ?)";
     $insertContainerStmt = $db->prepare($insertContainerQuery);
@@ -80,6 +120,29 @@ if (!empty($infoarray)) {
         $timeInSeconds = $info['time'];
         $formattedTime = gmdate("H:i:s", $timeInSeconds);
 
+        // Check if there are photos in the array
+        // if (!empty($info['photos'])) {
+        //     foreach ($info['photos'] as $photo) {
+        //         $fileName = $photo->name;
+        //         $fileSize = $photo->size;
+        //         $fileType = $photo->type;
+        //         $fileTmpName = $photo->tmp_name;
+        
+              
+        //         $newFileName = 'img/' . md5(time() . $fileName) . '.' . pathinfo($fileName, PATHINFO_EXTENSION);
+        //      $uploadPath = __DIR__ . '/' . $newFileName;
+        //         if (move_uploaded_file($fileTmpName, $uploadPath)) {
+              
+        //             $info['text'] .= " $newFileName";
+        //         } else {
+             
+        //             echo "File move failed.";
+        //         }
+
+        //         $info['text'] .= " $newFileName";
+        //     }
+        // }
+
         $insertContainerStmt->bind_param("iss", $lessonId, $info['text'], $formattedTime);
         $insertContainerStmt->execute();
     }
@@ -87,19 +150,20 @@ if (!empty($infoarray)) {
     $insertContainerStmt->close();
 }
 
+
 if (!empty($terminarray)) {
     $insertTerminQuery = "INSERT INTO lessontermins (lesson_id, text, time_in) VALUES (?, ?, ?)";
     $insertTerminStmt = $db->prepare($insertTerminQuery);
     foreach ($terminarray as $termin) {
         $timeInSeconds = $termin['time'];
         $formattedTime = gmdate("H:i:s", $timeInSeconds);
-    
+
         $insertTerminStmt->bind_param("iss", $lessonId, $termin['text'], $formattedTime);
         if (!$insertTerminStmt->execute()) {
             die("Ошибка выполнения запроса: " . $insertTerminStmt->error);
         }
     }
-    
+
     $insertTerminStmt->close();
 }
 
@@ -111,8 +175,16 @@ if (!empty($testarray)) {
         $timeInSeconds = $test['time'];
         $formattedTime = gmdate("H:i:s", $timeInSeconds);
 
-        $insertTestStmt->bind_param("sssssis", $test['question'], $test['incorrect1'], $test['incorrect2'], $test['incorrect3'],
-        $test['correctAnswer'], $lessonId, $formattedTime);
+        $insertTestStmt->bind_param(
+            "sssssis",
+            $test['question'],
+            $test['incorrect1'],
+            $test['incorrect2'],
+            $test['incorrect3'],
+            $test['correctAnswer'],
+            $lessonId,
+            $formattedTime
+        );
         $insertTestStmt->execute();
     }
 
@@ -125,8 +197,15 @@ if (!empty($finarray)) {
 
     foreach ($finarray as $fin) {
 
-        $insertFinStmt->bind_param("sssssi", $fin['finQuestion'], $fin['finIncorrect1'], $fin['finIncorrect2'], $fin['finIncorrect3'],
-        $fin['finCorrectAnswer'], $lessonId);
+        $insertFinStmt->bind_param(
+            "sssssi",
+            $fin['finQuestion'],
+            $fin['finIncorrect1'],
+            $fin['finIncorrect2'],
+            $fin['finIncorrect3'],
+            $fin['finCorrectAnswer'],
+            $lessonId
+        );
         $insertFinStmt->execute();
     }
 
@@ -144,5 +223,3 @@ if (!empty($link)) {
 
     $insertlinkStmt->close();
 }
-
-?>
